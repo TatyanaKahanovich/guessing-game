3d8e9099ce91dcf6a598ce31b86b8007bf1ecf90
Tatyana Kahanovich <d-m-tanya@yandex.ru>
2017-09-19 20:43:09 +0300

    guessing game result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.arr = [];
+        this.midpoint = 0;
+    }
 
     setRange(min, max) {
-
+        this.max = max;
+        for (var i = 0; i < this.max; i++) {
+             this.arr.push(i);
+        }
     }
 
     guess() {
-
+        this.midpoint = Math.round(this.arr.length / 2);
+        return this.arr[this.midpoint];
     }
 
     lower() {
-
+        this.arr.splice(this.midpoint, this.arr.length);
     }
 
     greater() {
+        this.arr.splice(0, this.midpoint);
 
     }
 }
